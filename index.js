4e9dda2bc7f3602c582bc3a2f7dcda82f98a33e2
JAOOUE\oauzx <Thitikorn.ba@gmail.com>
2025-11-18 13:29:23 +0700

    index fix commit

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-const express = require('express')
-const app = express()
-const PORT = 4000
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cookieSession = require('cookie-session');
 
-app.listen(PORT, () => {
-    console.log(`API Listening...`)
-})
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('This is my API')
-})
+app.use(cookieSession({
+  name: 'Session',
+  keys: ['key1', 'key2'],
+  maxAge: 3600 * 1000
+}))
 
-app.get('/about', (req, res) => {
-    res.send('This is my about')
-})
+const indexRouter = require('./routes/index');
+const getUserData  = require('./routes/getUserData');
+const adminRouter = require('./routes/admin');
+const authRouter = require('./routes/authPage');
 
-module.exports = app
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(getUserData);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
+
+
+app.use(function (req, res, next) {
+  return res.render('error');
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
