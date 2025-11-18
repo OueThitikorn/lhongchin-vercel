cd81437a7ebcf2ced3e78f6d2b034f26284a9653
JAOOUE\oauzx <Thitikorn.ba@gmail.com>
2025-11-18 13:39:58 +0700

    index fix commit

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ const cookieSession = require('cookie-session');
 
 const app = express();
 
-app.use(cookieSession({
-  name: 'Session',
-  keys: ['key1', 'key2'],
-  maxAge: 3600 * 1000
-}))
-
 const indexRouter = require('./routes/index');
 const getUserData  = require('./routes/getUserData');
 const adminRouter = require('./routes/admin');
@@ -19,12 +13,19 @@ const authRouter = require('./routes/authPage');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(getUserData);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cookieSession({
+    name: 'Session',
+    keys: ['key1', 'key2'],
+    maxAge: 3600 * 1000
+}))
+
+app.use(getUserData);
 
 app.use('/', indexRouter);
 app.use('/admin', adminRouter);
